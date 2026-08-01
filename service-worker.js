da47ea0a8d6d31b3dbdd69a9a73f1e8c934c5700
Romer De Mayo <romer.demayo@gmail.com>
2026-08-01 16:30:36 +1200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.14.0-dev-christian-creator-studio';
+const VERSION='1.14.0-dev-free-reel-audio-export';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
