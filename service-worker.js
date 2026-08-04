76bf2ba67dce4654c36ea3c55aff3399c26e51ae
Romer De Mayo <romer.demayo@gmail.com>
2026-08-04 16:20:03 +1200

    Refresh cache for My Library portrait layout

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.18.4-dev-sprint1-my-library';
+const VERSION='1.18.5-dev-library-mobile-layout';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
