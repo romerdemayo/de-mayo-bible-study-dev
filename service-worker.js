0690c906ce3f40ea33b864c0d26939716748bc19
Romer De Mayo <romer.demayo@gmail.com>
2026-08-05 22:31:06 +1200

    Build 1.23.2a performance cache refresh

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.23.2-dev-presentation-handout';
+const VERSION='1.23.2a-dev-performance';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
