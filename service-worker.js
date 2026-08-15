9a5ab576844a36e170a41fe37a38187a289b1bfb
Romer De Mayo <romer.demayo@gmail.com>
2026-08-15 16:46:06 +1200

    Bump cache for narration-synced slides

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.36-dev-resilient-motivation';
+const VERSION='1.27.37-dev-narration-synced-slides';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
