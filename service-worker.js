f9a2317f89f3191bd335cbc7f3e9ac2ff1d8742f
Romer De Mayo <romer.demayo@gmail.com>
2026-08-16 11:03:47 +1200

    Bump cache for two-minute narrated Reels

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.37-dev-narration-synced-slides';
+const VERSION='1.27.38-dev-two-minute-narrated-reels';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
