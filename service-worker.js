45d28698e2658fddf388af54fc190679ce05e177
Romer De Mayo <romer.demayo@gmail.com>
2026-08-16 12:23:32 +1200

    Bump cache for focused Reel hashtags

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.39-dev-fresh-reel-post-status';
+const VERSION='1.27.40-dev-focused-reel-hashtags';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
