fe330bc2c91bed0f119dfee6e46373882d24bb2f
Romer De Mayo <romer.demayo@gmail.com>
2026-08-29 20:30:39 +1200

    Bump cache for inline Reel copy actions

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.51-dev-reel-copy-toolbar';
+const VERSION='1.27.52-dev-inline-copy-actions';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
