63af1c468326d0e1a0b37066ceaa06b5b478797b
Romer De Mayo <romer.demayo@gmail.com>
2026-08-30 17:38:36 +1200

    Refresh cache for Surprise Me voiceover duration matching

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.60-dev-duration-content-sync';
+const VERSION='1.27.61-dev-surprise-voiceover-match';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
