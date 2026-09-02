47e60563ef507c98c5ca9aa456520a3d6eed5ff0
Romer De Mayo <romer.demayo@gmail.com>
2026-09-02 19:29:39 +1200

    Refresh offline Reel no-repeat cache

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.67-dev-offline-no-repeat';
+const VERSION='1.27.68-dev-offline-posted-lock';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
