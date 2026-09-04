3c14d89f23def809489050840c15df7bc5337a3e
Romer De Mayo <romer.demayo@gmail.com>
2026-09-04 22:16:16 +1200

    Bump cache for one-paste manual Reel

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Studies | Project Phoenix performance cache */
-const VERSION='1.27.72-dev-manual-reel';
+const VERSION='1.27.73-dev-one-paste-reel';
 const SHELL_CACHE=`de-mayo-shell-${VERSION}`;
 const RUNTIME_CACHE=`de-mayo-runtime-${VERSION}`;
 const OFFLINE_URL='./index.html';
